ee91869c27889e9ab8e7be3cffeaaa7d999e80cf
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-03-01 00:28:16 -0800

    finished

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,97 +3,190 @@ var timerEl = document.getElementById("timer");
 var input = document.getElementById("textinput");
 var startBtn = document.getElementById("startbutton");
 var resultMsg = document.getElementById("message");
-var choice1ht = document.querySelectorAll(".choice1");
-// var choice2ht = document.getElementById("choice2");
-// var choice3ht = document.getElementById("choice3");
 var choices = document.getElementById("choices");
-var j = 0;
-var i = 0;
+var title = document.getElementById("title");
+var form = document.getElementById("resultrecorder");
+var submitBtn = document.getElementById("submitbtn");
+var scorePage = document.getElementById("highrecord");
+var userInitial = document.querySelector("#user-initial");
+var resetBtn = document.getElementById("reset");
+var clearBtn = document.getElementById("clear");
+var storage = document.getElementById("clearStrg");
+var questioncount = 0;
+var score = 0;
+var timeleft = 100;
 
-
-var questionList = [
+var quizList = [
     {
-        question: "What is the name of our instructor ?",
-        answers:[
-            {
-                text: "Brian",
-                correct: true
-            },
-            {
-                text: "Jane",
-                correct: false
-            },
-            {
-                text: "Tom",
-                correct: false
-            },
-        ]
+        question: "What do you use to build website structure ?",
+        answer: ["HTML", "CSS", "Javascript"],
+        correctAnswer: "HTML",
     },
     {
-        question: "What is the name of our instructor ?",
-        answers:[
-            {
-                text: "Brian",
-                correct: true
-            },
-            {
-                text: "Jane",
-                correct: false
-            },
-            {
-                text: "Tom",
-                correct: false
-            },
-        ]
-    }
-];
+        question: "Who is our instructor ?",
+        answer: ["Brian", "Tom", "Jan"],
+        correctAnswer: "Brian",
+    },
+    {
+        question: "What is var stands for ?",
+        answer: ["Loop", "Variable", "Syntax"],
+        correctAnswer: "Variable",
+    },
+    {
+        question: "which of the following syntax is function ?",
+        answer: ["Let", "Const", "timeInterval()"],
+        correctAnswer: "timeInterval()",
+    },
+    {
+        question: "What are the ways to layout your page ?",
+        answer: ["Flexbox", "Bootstrap", "All above"],
+        correctAnswer: "All above",
+    }];
 
-// var quiz =[{
-//     question:"What is the name of our instructor ?",
-//     choice1:"Brian",
-//     choice2:"Jane",
-//     choice3:"Tom",
-//     correctAnswer:"Brian",
-// }];
+initialInput();
 
- var countdown = function(){
-     var timeleft = 100;
-     var timeInterval = setInterval(function(){
-         timeleft--;
-         timerEl.textContent = timeleft + " seconds";
-         if(timeleft === 0){
-             clearInterval(timeInterval);
-             timerEl.style.display = "none";
-         };
-     },1000);
- };
+var countdown = function () {
+    timeInterval = setInterval(function () {
+        if (timeleft > 0) {
+            timeleft--;
+            timerEl.textContent = timeleft + " seconds";
+            // if (timeleft === 0) {
+            //     clearInterval(timeInterval);
+            //     timerEl.style.display = "none"
+            // };
+        } else {
+            timerFinish();
+            quizOver();
+        };
 
-
- var displayQs = function(){
-    input.textContent = questionList[j].question;
-    choices.style.display = "block";
-    for (var i=0;i<questionList[j].answers.length; i++){
-        choice1ht.textContent=questionList[j].answers[i].text;
-    }
- };
-
-
-function answerCheck(){
-    if(questionList[j].correctAnswer){
-        console.log(correctAnswer);
-        resultMsg.textContent = "Correct!!";
-    }
-    displayQs();
+    }, 1000);
 };
 
+function timerFinish(){
+    clearInterval(timeInterval);
+    timerEl.textContent = "Count Down Ends.";
+}
 
-startBtn.addEventListener("click",function(){
-     startBtn.style.display = "none";
-     displayQs();
-     countdown();
- });
+var displayQs = function () {
+    resultMsg.textContent = "";
+    var entireQuestion = quizList[questioncount];
 
-choices.addEventListener("click",function(){
-    answerCheck();
-    console.log("click");
+    input.textContent = entireQuestion.question;
+
+    choices.innerHTML = "";
+
+    for (var i = 0; i < entireQuestion.answer.length; i++) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("value", entireQuestion.answer[i]);
+        choiceBtn.setAttribute("class", "btns")
+        choiceBtn.textContent = entireQuestion.answer[i];
+        choiceBtn.onclick = answerCheck;
+        choices.appendChild(choiceBtn);
+    }
+};
+
+function highScore() {
+    scoreEl.textContent = "Score: " + score;
+};
+
+function answerCheck() {
+    console.log(quizList[questioncount].correctAnswer);
+    if (this.value !== quizList[questioncount].correctAnswer) {
+        // resultMsg.textContent = "Wrong!!";
+        timeleft -= 20;
+    } else if (this.value === quizList[questioncount].correctAnswer) {
+        // resultMsg.textContent = "Correct!!";
+        score += 20;
+        scoreEl.textContent = "Score: "+ score;
+    }
+    questioncount++;
+    if (questioncount === quizList.length) {
+        quizOver();
+        timerFinish();
+    } else {
+        displayQs();
+    }
+};
+
+function quizOver() {
+    clearInterval(timeInterval);
+    title.textContent = "Quiz Over!!"
+    input.style.display = "none";
+    choices.style.display = "none";
+    form.removeAttribute("style", "block");
+    highScore();
+}
+
+function initialInput() {
+    var initial = localStorage.getItem("initial");
+
+    if (!initial) {
+        return;
+    }
+    userInitial.textContent = initial + ", Score: " + score;
+}
+
+function setInitial(){
+    var initial = document.querySelector("#record").value;
+
+    if (initial === "") {
+        displayMessage("error", "initial cannot be blank");
+    } else {
+        displayMessage("success", "Input successfully");
+
+        localStorage.setItem("initial", initial);
+        initialInput();
+    };
+}
+
+function displayMessage(type, message) {
+    resultMsg.textContent = message;
+    resultMsg.setAttribute("class", type);
+}
+
+function restart(){
+    clearInterval(timeInterval);
+    score = 0;
+    questioncount = 0;
+    timeleft = 100;
+}
+
+
+startBtn.addEventListener("click", function () {
+    startBtn.style.display = "none";
+    choices.style.display = "block";
+    displayQs();
+    countdown();
 });
+
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    scorePage.style.display = "block";
+    form.style.display = "none";
+    title.style.display = "none";
+    clearBtn.style.display = "block";
+    resetBtn.style.display = "block";
+    setInitial();
+})
+
+clearBtn.addEventListener("click",function(){
+    restart();
+    localStorage.clear();
+    storage.style.display = "none";
+    resultMsg.style.display = "none";
+})
+
+resetBtn.addEventListener("click",function(){
+    resetBtn.style.display = "none";
+    clearBtn.style.display = "none";
+    scorePage.style.display = "none";
+    resultMsg.style.display = "none";
+    title.style.display = "block";
+    input.style.display = "block";
+    startBtn.style.display = "block";
+    title.textContent = "Code Quiz!!";
+    scoreEl.textContent = "Score Result";
+    timerEl.textContent = "Timer";
+    input.textContent = "This is a quiz to quiz your knowledge of our class :)";
+    restart();
+})
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# HW04
+### 4. Web APIs: Code Quiz
+
+Create a Code Quiz.  
+
+
+### Motivation
+
+We had learn basic functions we can use to communicate with our page by using Javascript. As we dig in more on DOM using javascript. This hw practice to understanding how to combined all the function we had learn in the class practice to make a mutiple choice quiz. Since this one had more variables to set up to make sure you get all the things to store at right place.
+
+
+Screenshot
+
+![Screenshot of HW website](/Assets/hw04website.png)
+
+#### Code style
+
+Javascript, HTML and CSS
+
+
+### Summary
+
+I had set up an Array which also contain another array in side it with the question's choices. Using creat element to insert the button while impilying the text for each question which get rotated to. Using addeventlistenr to give the button can be clickable so that we can transfer to next question or next screen when its needed. After the question is done and make it show up the score display page and also have set up an input for user to enter their initial. Once they submitted the initial we set up the function to store it in the local storage and make it display with their initial and the score they earned.
+
+
+### Link
+
+Click here: [ASSMT#4 LINK](https://anniechen9025.github.io/HW04/) 
+
